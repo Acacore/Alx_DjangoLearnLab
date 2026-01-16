92e5856e08e1ccaf140cd04a9a6a15a6ffa79d04
Acacore <mensart81@outlook.com>
2026-01-16 16:48:48 +0000

    template name altered

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -10,11 +10,11 @@ def home(request):
 
 def book_list(request):
     books = Book.objects.all()
-    return render(request, "relationship_app/book_list.html", {"books": books})
+    return render(request, "relationship_app/list_book.html", {"books": books})
 
 def book_detail(request, pk):
     book = Book.objects.get(pk=pk)
-    return render(request, "relationship_app/book_detail.html", {"book": book})
+    return render(request, "relationship_app/detail_book.html", {"book": book})
 
 
 
