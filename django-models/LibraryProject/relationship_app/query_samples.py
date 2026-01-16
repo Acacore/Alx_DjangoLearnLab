41ec059292c41eadf4aea3273a716752fddb9fea
Acacore <mensart81@outlook.com>
2026-01-16 16:01:38 +0000

    Add query_samples script for model relationship queries

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,6 +1,38 @@
-from .models import *
+import os
+import django
+
+# Set up Django environment
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LibraryProject.settings')
+django.setup()
+
+from relationship_app.models import Author, Book, Library, Librarian
+
+# Sample queries demonstrating relationships
+def query_books_by_author(author_name):
+    """
+    Query all books by a specific author
+    """
+    return Book.objects.filter(author__name=author_name)
+
+# List all books in a specific library
+def list_books_in_library(library_name):
+    """
+    List all books in a library
+    """
+    library = Library.objects.get(name=library_name)
+    return library.books.all()
+
+# Get the librarian for a specific library
+def get_librarian_for_library(library_name):
+    """
+    Retrieve the librarian for a library
+    """
+    library = Library.objects.get(name=library_name)
+    return library.librarian
 
 
-books = Book.objects.filter(author__name="J.K. Rowling")
-libraries = Library.objects.filter(books__title="1984")
-librarian = Libarian.objects.get(library__name="Central Library")
+# Example usage
+if __name__ == "__main__":
+    print(query_books_by_author("Chinua Achebe"))
+    print(list_books_in_library("Central Library"))
+    print(get_librarian_for_library("Central Library"))
