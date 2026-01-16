9f7c74a7e943cfd21c37cd699f474fef19e2912c
Acacore <mensart81@outlook.com>
2026-01-16 16:16:59 +0000

    Fix query_samples get_librarian_for_library to match ALX requirements

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -29,7 +29,7 @@ def get_librarian_for_library(library_name):
     Retrieve the librarian for a library
     """
     library = Library.objects.get(name=library_name)
-    return library.librarian
+    return Librarian.objects.get(library=library)
 
 
 # Example usage
