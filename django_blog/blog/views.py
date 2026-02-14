1345b9467dcdc55c4322873f81b4f95297662b4c
Acacore <mensart81@outlook.com>
2026-02-14 22:34:46 +0000

    Add user profile model and auto-create profile using signals

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'blog/home.html')
+
+
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = CustomUserCreationForm()
+
+    return render(request, 'registration/register.html', {'form': form})
