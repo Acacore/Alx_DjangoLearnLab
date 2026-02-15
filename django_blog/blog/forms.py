08f94e7213871a330d8e8bd10feca8b2de1ef4c8
Acacore <mensart81@outlook.com>
2026-02-15 19:32:14 +0000

    git commit -m 'Implement PostForm with TagWidget, tag handling, and title validation'

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,6 +2,27 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Post, Comment, Tag
+from django.forms.widgets import TextInput
+from django.utils.html import format_html
+
+
+
+class TagWidget(TextInput):
+    """
+    A custom widget for django-taggit tag fields.
+    Renders a text input where multiple tags can be entered
+    as a comma-separated string.
+    """
+
+    def render(self, name, value, attrs=None, renderer=None):
+        if value is None:
+            value = ""
+        elif isinstance(value, list):
+            # convert list of tags into a comma-separated string
+            value = ", ".join([str(v) for v in value])
+
+        return super().render(name, value, attrs, renderer)
+
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -23,6 +44,13 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
+        widgets = {
+            'tags': TagWidget(attrs={
+                'placeholder': 'Add tags separated by commas',
+                'class': 'form-control'  # optional, for bootstrap styling
+            })
+        }
+
 
     def clean_title(self):
         title = self.cleaned_data.get("title")
@@ -42,4 +70,5 @@ class PostForm(forms.ModelForm):
             instance.tags.add(tag)
 
         return instance
+
 
